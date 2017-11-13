36d1e587fdb8efa18bf520532bbf0d3e11bb58a8
alfuans <alfuans@gmail.com>
2017-11-13 11:42:33 +0100

    Edition photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,6 +4,11 @@ class PhotosController < InheritedResources::Base
     @photos = Photo.all.paginate(page: params[:page], per_page: 4)
   end
   
+  def new
+    @photo = Photo.new(:album_id => params[:album_id])
+    @album = Album.find(params[:album_id])
+  end
+  
   private
 
     def photo_params
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,7 @@ class Photo < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   validates :name, presence: true, length: {minimum: 2}
+  validates :image, presence: true
   validates :image, file_size: { less_than: 1.megabytes }
     
 end
